73b804cb8a9a982b7f43a941da6df3591195a5c5
KeslieBox <kesliebox@gmail.com>
2021-03-17 18:28:47 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,24 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist 
+  belongs_to :genre 
+  has_many :notes 
+  validates :title, presence: true
+
+
+  def artist_name=(name)
+    self.create_artist(name: name)
+  end
+
+  def artist_name
+    self.artist && self.artist.name
+  end
+
+ def notes=(notes)
+  notes.each do |n|
+    if !n.empty? 
+      self.notes << Note.create(content: n)
+    end
+  end
+ end
 end
